99a2050d826f13cabc1d584c2fd16e1ed98a3a85
Gabriel Magen <gabriel.magen@gmail.com>
2020-02-24 04:56:09 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function() {
+        $(".container").scrollTop(500);
+      });
+
+      function myFunction() {
+        location.reload();
+      }
+      //------------------------------------------------------
+      setTimeout(function() {
+        location.reload();
+      }, 900000);
+      //------------------------------------------------------
